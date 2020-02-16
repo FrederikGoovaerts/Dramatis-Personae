cb5fb08b7a7fbeebdec6f0515be1267e4032da9a
Frederik Goovaerts <info@frederik.dev>
2020-02-16 23:38:35 +0100

    Add configuration object for Google OpenID client

---
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/Configurations.kt
@@ -0,0 +1,12 @@
+package dev.frederik.dramatispersonae
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AuthenticationConfig {
+    @Value("\${spring.googleAuth.clientId}")
+    val clientId: String = ""
+    @Value("\${spring.googleAuth.clientSecret}")
+    val clientSecret: String = ""
+}
