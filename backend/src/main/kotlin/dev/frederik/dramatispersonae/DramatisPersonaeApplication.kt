b183d71fabc3601ee2d40e21c888141ab5042177
Frederik Goovaerts <info@frederik.dev>
2020-02-16 21:46:47 +0100

    Add local development fixtures tied to dev spring profile

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/DramatisPersonaeApplication.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/DramatisPersonaeApplication.kt
@@ -1,11 +1,29 @@
 package dev.frederik.dramatispersonae
 
+import dev.frederik.dramatispersonae.model.Campaign
+import dev.frederik.dramatispersonae.model.CampaignRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class DramatisPersonaeApplication
 
 fun main(args: Array<String>) {
 	runApplication<DramatisPersonaeApplication>(*args)
+}
+
+@Component
+@Profile("dev-local")
+class LocalDevFixtures (
+	private val campaignRepository: CampaignRepository
+): CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+		val campaign1 = Campaign("Test camp1")
+		this.campaignRepository.save(campaign1)
+	}
+
 }
