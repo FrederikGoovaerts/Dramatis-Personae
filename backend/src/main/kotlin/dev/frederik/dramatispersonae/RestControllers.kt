8a2e0e458250409b8ef46dc776bd43311ec6c89e
Frederik Goovaerts <info@frederik.dev>
2020-02-17 23:08:48 +0100

    Add authentication endpoint for google oauth authentication

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/RestControllers.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/RestControllers.kt
@@ -1,7 +1,11 @@
 package dev.frederik.dramatispersonae
 
+import dev.frederik.dramatispersonae.auth.GoogleOpenIdClient
+import dev.frederik.dramatispersonae.auth.TokenSet
 import dev.frederik.dramatispersonae.model.Campaign
 import dev.frederik.dramatispersonae.model.CampaignRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 class CreateCampaignDto(var name: String)
@@ -19,4 +23,23 @@ class ArticleController(private val repository: CampaignRepository) {
         this.repository.save(newCampaign)
     }
 
+}
+
+data class AuthenticationCodeDto(val code: String, val redirectUri: String)
+data class RefreshTokenDto(val token: String)
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthenticationController @Autowired constructor(authenticationConfig: AuthenticationConfig) {
+    private val googleOpenIdClient: GoogleOpenIdClient = GoogleOpenIdClient(authenticationConfig)
+
+    @PostMapping("/code")
+    fun getTokenSet(@Validated @RequestBody dto: AuthenticationCodeDto): TokenSet {
+        return googleOpenIdClient.exchangeCodeForTokenSet(dto.code, dto.redirectUri)
+    }
+
+    @PostMapping("/refresh")
+    fun refreshTokenSet(@Validated @RequestBody dto: RefreshTokenDto): TokenSet {
+        return googleOpenIdClient.refreshTokenSet(dto.token)
+    }
 }
