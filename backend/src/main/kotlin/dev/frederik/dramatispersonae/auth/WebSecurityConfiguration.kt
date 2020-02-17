ef0648d34b0dad1494d6ebcc74d7b76806632675
Frederik Goovaerts <info@frederik.dev>
2020-02-17 23:32:38 +0100

    Add separate web security for light dev mode

---
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/auth/WebSecurityConfiguration.kt
@@ -0,0 +1,38 @@
+package dev.frederik.dramatispersonae.auth
+
+import dev.frederik.dramatispersonae.AuthenticationConfig
+import dev.frederik.dramatispersonae.model.UserRepository
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+@Profile("!dev-light")
+class WebSecurity(private val userRepository: UserRepository,
+                  private val authenticationConfig: AuthenticationConfig) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable().authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS).permitAll()
+            .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(JwtAuthorizationFilter(authenticationManager(), authenticationConfig, userRepository))
+    }
+
+}
+
+@EnableWebSecurity
+@Profile("dev-light")
+class WebSecurityDevLocal(private val userRepository: UserRepository,
+                  private val authenticationConfig: AuthenticationConfig) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and()
+                .addFilter(JwtAuthorizationFilter(authenticationManager(), authenticationConfig, userRepository))
+    }
+
+}
