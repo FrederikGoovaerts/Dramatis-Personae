318506d9f4506fbb42d1e8225c92b45c9bc5ecf1
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2020-02-22 00:10:49 +0100

    Add some fixtures for dev testing

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/DramatisPersonaeApplication.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/DramatisPersonaeApplication.kt
@@ -2,6 +2,8 @@ package dev.frederik.dramatispersonae
 
 import dev.frederik.dramatispersonae.model.Campaign
 import dev.frederik.dramatispersonae.model.CampaignRepository
+import dev.frederik.dramatispersonae.model.User
+import dev.frederik.dramatispersonae.model.UserRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -20,11 +22,18 @@ fun main(args: Array<String>) {
  */
 @Component
 @Profile("dev")
-class LocalDevFixtures (private val campaignRepository: CampaignRepository): CommandLineRunner {
+class LocalDevFixtures (private val userRepository: UserRepository,
+						private val campaignRepository: CampaignRepository): CommandLineRunner {
 
 	override fun run(vararg args: String?) {
-		val campaign1 = Campaign("Test camp1")
+		val user1 = User("1", "Name1", "Email1")
+		this.userRepository.save(user1)
+		val user2 = User("2", "Name2", "Email2")
+		this.userRepository.save(user2)
+		val campaign1 = Campaign("Camp1", user1, mutableListOf(user1, user2), mutableListOf())
 		this.campaignRepository.save(campaign1)
+		val campaign2 = Campaign("Camp2", user2, mutableListOf(user2), mutableListOf())
+		this.campaignRepository.save(campaign2)
 	}
 
 }
