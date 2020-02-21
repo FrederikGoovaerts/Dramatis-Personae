a2e25ee4e8e0e2ff9875df8146506d8461a9b38c
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2020-02-22 00:10:37 +0100

    Split RestControllers file into services

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/service/AuthService.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/service/AuthService.kt
@@ -1,30 +1,14 @@
-package dev.frederik.dramatispersonae
+package dev.frederik.dramatispersonae.service
 
-import dev.frederik.dramatispersonae.auth.GoogleAuthentication
+import dev.frederik.dramatispersonae.AuthenticationConfig
 import dev.frederik.dramatispersonae.auth.GoogleOpenIdClient
 import dev.frederik.dramatispersonae.auth.TokenSet
-import dev.frederik.dramatispersonae.model.Campaign
-import dev.frederik.dramatispersonae.model.CampaignRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
-
-class CreateCampaignDto(var name: String)
-
-@RestController
-@RequestMapping("/api/campaign")
-class CampaignController(private val repository: CampaignRepository) {
-
-    @GetMapping("/")
-    fun getCampaigns() = repository.findAll()
-
-    @PostMapping("/")
-    fun postCampaign(auth: GoogleAuthentication, @RequestBody campaign: CreateCampaignDto) {
-        val newCampaign = Campaign(campaign.name)
-        this.repository.save(newCampaign)
-    }
-
-}
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 data class AuthenticationCodeDto(val code: String, val redirectUri: String)
 data class RefreshTokenDto(val token: String)
