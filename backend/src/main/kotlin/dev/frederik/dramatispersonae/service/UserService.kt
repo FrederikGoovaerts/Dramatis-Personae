c1a58f39f2b2c3ad17c14873375653de1c65af90
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2020-02-22 21:52:37 +0100

    Add user details call

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/service/UserService.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/service/UserService.kt
@@ -1,3 +1,16 @@
 package dev.frederik.dramatispersonae.service
 
-data class UserListView(val name: String)
+import dev.frederik.dramatispersonae.auth.GoogleAuthentication
+import dev.frederik.dramatispersonae.model.UserRepository
+import org.springframework.web.bind.annotation.*
+
+data class UserView(val name: String, val email: String)
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(val userRepository: UserRepository) {
+
+    @GetMapping("/")
+    fun getSelf(auth: GoogleAuthentication) = UserView(auth.principal.fullName, auth.principal.email)
+
+}
