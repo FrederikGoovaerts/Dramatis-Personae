5bbab0d8f43dd61a7c209301ec8895a021c0f240
Frederik Goovaerts <info@frederik.dev>
2020-02-22 22:00:04 +0100

    Add CreateCharacterDto

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/service/CharacterService.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/service/CharacterService.kt
@@ -2,4 +2,6 @@ package dev.frederik.dramatispersonae.service
 
 import java.util.*
 
+data class CreateCharacterDto(val name: String, val description: String)
+
 data class CharacterListView(val name: String, val id: UUID)
