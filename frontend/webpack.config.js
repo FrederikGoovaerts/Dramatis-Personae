4836a0a59d70c39da93c93b359de86dfb4406d84
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2020-02-27 00:22:41 +0100

    Reformat webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,58 +1,54 @@
-const DotEnv = require("dotenv-webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotEnv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    entry: "./src/index.tsx",
+    entry: './src/index.tsx',
     output: {
-        filename: "bundle.[hash].js",
+        filename: 'bundle.[hash].js',
         path: `${__dirname}/build`
     },
 
-    devtool: "source-map",
+    devtool: 'source-map',
 
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.json']
     },
 
     module: {
         rules: [
-            { test: /\.js$/, enforce: "pre", loader: "source-map-loader" },
-            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.js$/, enforce: 'pre', loader: 'source-map-loader' },
+            { test: /\.tsx?$/, loader: 'ts-loader' },
             {
                 test: /\.scss$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                ]
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
             },
-            { test: /\.svg$/, loader: "raw-loader" }
+            { test: /\.svg$/, loader: 'raw-loader' }
         ]
     },
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: "src/index.html"
+            template: 'src/index.html'
         })
     ],
 
     optimization: {
         usedExports: true,
         splitChunks: {
-            chunks: "all"
+            chunks: 'all'
         }
     }
 };
 
 module.exports = (env, argv) => {
-    if (argv.mode === "development") {
+    if (argv.mode === 'development') {
         config.devServer = {
             open: true,
-            historyApiFallback: true
+            historyApiFallback: true,
         };
         config.plugins.push(
             new DotEnv({
-                path: "./.env"
+                path: './.env'
             })
         );
     }
