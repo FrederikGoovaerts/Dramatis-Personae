4781a51582025c2d4bc5d79e2ed5a0fc7612bed6
Frederik Goovaerts <info@frederik.dev>
2020-04-13 21:12:48 +0200

    Restore campaign list sagas

--- frontend/src/store/sagas/campaign.sagas.ts
+++ frontend/src/store/sagas/campaign.sagas.ts
@@ -14,24 +14,24 @@ function* fetchCampaigns() {
     yield put(campaignActions.actions.setCampaignsLoading(false));
 }
 
-// function* newCampaign(action: campaignActions.specificTypes['newCampaign']) {
-//     try {
-//         const prototype: CampaignPrototype = { name: action.payload };
-//         yield campaign.create(prototype);
-//         yield put(campaignActions.actions.fetchCampaigns());
-//     } catch (e) {
-//         console.error('Unable to create campaign. Please try again later.');
-//     }
-// }
+function* joinCampaign(action: campaignActions.specificTypes['joinCampaign']) {
+    try {
+        yield campaign.join(action.payload);
+        yield put(campaignActions.actions.fetchCampaigns());
+    } catch (e) {
+        console.error('Unable to join campaign. Please try again later or check if the code is correct.');
+    }
+}
 
-// function* joinCampaign(action: campaignActions.specificTypes['joinCampaign']) {
-//     try {
-//         yield campaign.join(action.payload);
-//         yield put(campaignActions.actions.fetchCampaigns());
-//     } catch (e) {
-//         console.error('Unable to create campaign. Please try again later.');
-//     }
-// }
+function* newCampaign(action: campaignActions.specificTypes['newCampaign']) {
+    try {
+        const prototype: CampaignPrototype = { name: action.payload };
+        yield call(campaign.create, prototype);
+        yield put(campaignActions.actions.fetchCampaigns());
+    } catch (e) {
+        console.error('Unable to create campaign. Please try again later.');
+    }
+}
 
 // function* deleteCampaign(action: campaignActions.specificTypes['deleteCampaign']) {
 //     try {
@@ -44,7 +44,7 @@ function* fetchCampaigns() {
 
 export default function* watcher() {
     yield takeEvery(campaignActions.names.fetchCampaigns, fetchCampaigns);
-    // yield takeEvery(campaignActions.names.newCampaign, newCampaign);
-    // yield takeEvery(campaignActions.names.joinCampaign, joinCampaign);
+    yield takeEvery(campaignActions.names.joinCampaign, joinCampaign);
+    yield takeEvery(campaignActions.names.newCampaign, newCampaign);
     // yield takeEvery(campaignActions.names.deleteCampaign, deleteCampaign);
 }
