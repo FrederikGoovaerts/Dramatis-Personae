6d919ed119e1cfad19cf67e9a593277f9cfce277
Frederik Goovaerts <info@frederik.dev>
2020-04-15 22:26:47 +0200

    Start work on Character Detail

--- frontend/src/store/actions/character.actions.ts
+++ frontend/src/store/actions/character.actions.ts
@@ -3,7 +3,8 @@ import {
     CharacterDeletePayload,
     CharacterUpdatePayload,
     CreateNotePayload,
-    VisibilityUpdatePayload
+    VisibilityUpdatePayload,
+    Note
 } from '../../types';
 import { ActionTypeMapping, ActionUnion, createAction } from './base';
 
@@ -11,9 +12,11 @@ export enum names {
     fetchCharacter = 'FETCH_CHARACTER',
     setCharacterLoading = 'SET_CHARACTER_LOADING',
     setCharacter = 'SET_CHARACTER',
+    fetchNotes = 'FETCH_NOTES',
+    setNotesLoading = 'SET_NOTES_LOADING',
+    setNotes = 'SET_NOTES',
     updateCharacter = 'UPDATE_CHARACTER',
     deleteCharacter = 'DELETE_CHARACTER',
-    setNote = 'SET_CHARACTER_NOTE',
     setVisibility = 'SET_CHARACTER_VISIBILITY'
 }
 
@@ -21,9 +24,11 @@ export const actions = {
     fetchCharacter: (id: string) => createAction(names.fetchCharacter, id),
     setCharacterLoading: (p: boolean) => createAction(names.setCharacterLoading, p),
     setCharacter: (p: Character) => createAction(names.setCharacter, p),
+    fetchNotes: (id: string) => createAction(names.fetchNotes, id),
+    setNotesLoading: (p: boolean) => createAction(names.setNotesLoading, p),
+    setNotes: (p: Note[]) => createAction(names.setNotes, p),
     updateCharacter: (p: CharacterUpdatePayload) => createAction(names.updateCharacter, p),
     deleteCharacter: (p: CharacterDeletePayload) => createAction(names.deleteCharacter, p),
-    setNote: (p: CreateNotePayload) => createAction(names.setNote, p),
     setVisible: (p: VisibilityUpdatePayload) => createAction(names.setVisibility, p)
 };
 
