09257ac62c30ce3e033aba9906e191913c79656d
Frederik Goovaerts <info@frederik.dev>
2020-04-19 22:11:51 +0200

    Clean up character sagas

--- frontend/src/store/sagas/character.sagas.ts
+++ frontend/src/store/sagas/character.sagas.ts
@@ -1,6 +1,5 @@
 import { put, takeEvery } from 'redux-saga/effects';
-import { Character } from '../../types';
-import { campaignActions, characterActions } from '../actions';
+import { characterActions } from '../actions';
 import * as character from '../../api/character.api';
 
 function* fetchCharacter(action: characterActions.specificTypes['fetchCharacter']) {
@@ -18,22 +17,12 @@ function* fetchNotes(action: characterActions.specificTypes['fetchNotes']) {
     yield put(characterActions.actions.setNotesLoading(true));
     try {
         const result = yield character.getNotes(action.payload);
-        console.log(result);
         yield put(characterActions.actions.setNotes(result));
     } catch (e) {
         console.error('Unable to fetch notes. Please try again later.');
     }
     yield put(characterActions.actions.setNotesLoading(false));
 }
-
-// function* newCharacter(action: characterActions.specificTypes['newCharacter']) {
-//     try {
-//         yield character.create(action.payload);
-//         yield put(campaignActions.actions.fetchCampaign(String(action.payload.campaignId)));
-//     } catch (e) {
-//         console.error('Unable to create character. Please try again later.');
-//     }
-// }
 
 // function* updateCharacter(action: characterActions.specificTypes['updateCharacter']) {
 //     try {
@@ -75,7 +64,6 @@ function* setVisibility(action: characterActions.specificTypes['setVisible']) {
 export default function* watcher() {
     yield takeEvery(characterActions.names.fetchCharacter, fetchCharacter);
     yield takeEvery(characterActions.names.fetchNotes, fetchNotes);
-    // yield takeEvery(characterActions.names.newCharacter, newCharacter);
     // yield takeEvery(characterActions.names.updateCharacter, updateCharacter);
     // yield takeEvery(characterActions.names.deleteCharacter, deleteCharacter);
     // yield takeEvery(characterActions.names.setNote, setNote);
