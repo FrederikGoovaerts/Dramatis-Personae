dadbf477cb36cb6b43ab8be7a24d8973f4930884
Frederik Goovaerts <info@frederik.dev>
2020-04-26 23:42:36 +0200

    Implement logic for edit and delete character

--- frontend/src/store/sagas/character.sagas.ts
+++ frontend/src/store/sagas/character.sagas.ts
@@ -1,5 +1,5 @@
 import { put, takeEvery } from 'redux-saga/effects';
-import { characterActions } from '../actions';
+import { characterActions, campaignActions } from '../actions';
 import * as character from '../../api/character.api';
 
 function* fetchCharacter(action: characterActions.specificTypes['fetchCharacter']) {
@@ -24,23 +24,22 @@ function* fetchNotes(action: characterActions.specificTypes['fetchNotes']) {
     yield put(characterActions.actions.setNotesLoading(false));
 }
 
-// function* updateCharacter(action: characterActions.specificTypes['updateCharacter']) {
-//     try {
-//         yield character.update(action.payload.characterId, action.payload.update);
-//         yield put(characterActions.actions.fetchCharacter(String(action.payload.characterId)));
-//     } catch (e) {
-//         console.error('Unable to update character. Please try again later.');
-//     }
-// }
+function* editCharacter(action: characterActions.specificTypes['editCharacter']) {
+    try {
+        yield character.update(action.payload.characterId, action.payload);
+        yield put(characterActions.actions.fetchCharacter(String(action.payload.characterId)));
+    } catch (e) {
+        console.error('Unable to update character. Please try again later.');
+    }
+}
 
-// function* deleteCharacter(action: characterActions.specificTypes['deleteCharacter']) {
-//     try {
-//         yield character.deletePermanently(action.payload.characterId);
-//         yield put(campaignActions.actions.fetchCampaign(String(action.payload.campaignId)));
-//     } catch (e) {
-//         console.error('Unable to delete character. Please try again later.');
-//     }
-// }
+function* deleteCharacter(action: characterActions.specificTypes['deleteCharacter']) {
+    try {
+        yield character.deletePermanently(action.payload);
+    } catch (e) {
+        console.error('Unable to delete character. Please try again later.');
+    }
+}
 
 function* createNote(action: characterActions.specificTypes['createNote']) {
     try {
@@ -63,8 +62,8 @@ function* setVisibility(action: characterActions.specificTypes['setVisible']) {
 export default function* watcher() {
     yield takeEvery(characterActions.names.fetchCharacter, fetchCharacter);
     yield takeEvery(characterActions.names.fetchNotes, fetchNotes);
-    // yield takeEvery(characterActions.names.updateCharacter, updateCharacter);
-    // yield takeEvery(characterActions.names.deleteCharacter, deleteCharacter);
+    yield takeEvery(characterActions.names.editCharacter, editCharacter);
+    yield takeEvery(characterActions.names.deleteCharacter, deleteCharacter);
     yield takeEvery(characterActions.names.createNote, createNote);
     yield takeEvery(characterActions.names.setVisibility, setVisibility);
 }
