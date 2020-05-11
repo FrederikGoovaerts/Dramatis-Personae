fb80f529f9f795c3a5a32f06ff4d968f143b150d
Frederik Goovaerts <info@frederik.dev>
2020-05-11 23:19:14 +0200

    Add leave button to campaign detail

--- frontend/src/components/pages/CampaignDetailScreen.tsx
+++ frontend/src/components/pages/CampaignDetailScreen.tsx
@@ -20,6 +20,7 @@ import { CampaignCharacterBreadcrumb } from '../molecules/CampaignCharacterBread
 import { Header } from '../molecules/Header';
 import { NewCharacterForm } from '../molecules/NewCharacterForm';
 import { EditCampaignForm } from '../molecules/EditCampaignForm';
+import { ConfirmableButton } from '../atoms/DeleteButton';
 
 export interface MatchParams {
     id: string;
@@ -36,6 +37,7 @@ interface MapProps {
     fetchCampaign: (id: string) => void;
     fetchCharacters: (campaignId: string) => void;
     deleteCampaign: (id: string) => void;
+    leaveCampaign: (id: string) => void;
 }
 
 type AllProps = Props & MapProps;
@@ -44,13 +46,13 @@ interface State {
     createOpen: boolean;
     editCampaignOpen: boolean;
     deleteCheck: boolean;
-    deleted: boolean;
+    inaccessible: boolean;
 }
 
 class CampaignDetailRaw extends React.Component<AllProps, State> {
     constructor(props: AllProps) {
         super(props);
-        this.state = { createOpen: false, editCampaignOpen: false, deleteCheck: false, deleted: false };
+        this.state = { createOpen: false, editCampaignOpen: false, deleteCheck: false, inaccessible: false };
     }
 
     componentDidMount(): void {
@@ -69,6 +71,15 @@ class CampaignDetailRaw extends React.Component<AllProps, State> {
     closeEditCampaign = (): void => {
         this.setState({ editCampaignOpen: false });
     };
+
+    leaveCampaign = (): void => {
+        if (this.props.campaign) {
+            this.props.leaveCampaign(this.props.campaign.id);
+            this.makeInaccessible();
+        }
+    };
+
+    makeInaccessible = (): void => this.setState({ inaccessible: true });
 
     renderCharacter = (character: ListCharacter) => (
         <ListItemLink to={`${this.props.match.url}${routes.character}${character.id}`} key={character.id}>
@@ -92,19 +103,18 @@ class CampaignDetailRaw extends React.Component<AllProps, State> {
         if (!this.props.campaign) {
             return <div />;
         }
-        const onDelete = () => this.setState({ deleted: true });
         return (
             <EditCampaignForm
                 id={this.props.match.params.id}
                 name={this.props.campaign?.name}
                 onSubmitComplete={this.closeEditCampaign}
-                onDelete={onDelete}
+                onDelete={this.makeInaccessible}
             />
         );
     };
 
     render() {
-        if (this.state.deleted) {
+        if (this.state.inaccessible) {
             return (
                 <div>
                     <Redirect to={routes.root} />
@@ -148,10 +158,18 @@ class CampaignDetailRaw extends React.Component<AllProps, State> {
                             <Typography variant="caption">Invite code: {this.props.campaign.inviteCode}</Typography>
                         </Box>
                     )}
-                    {this.props.campaign.owner && (
+                    {this.props.campaign.owner ? (
                         <Fab className="CampaignDetail__createFab" color="primary" onClick={this.openCreate}>
                             <Add />
                         </Fab>
+                    ) : (
+                        <Box marginTop="1em">
+                            <ConfirmableButton
+                                onConfirm={this.leaveCampaign}
+                                defaultText="Leave campaign"
+                                confirmedText="Leaving..."
+                            />
+                        </Box>
                     )}
 
                     <Modal open={this.state.createOpen} onClose={this.closeCreate}>
@@ -181,5 +199,6 @@ const mapStateToProps = (state: RootState) => ({
 export const CampaignDetailScreen = connect(mapStateToProps, {
     fetchCampaign: campaignActions.actions.fetchCampaign,
     fetchCharacters: campaignActions.actions.fetchCharacters,
-    deleteCampaign: campaignActions.actions.deleteCampaign
+    deleteCampaign: campaignActions.actions.deleteCampaign,
+    leaveCampaign: campaignActions.actions.leaveCampaign
 })(CampaignDetailRaw);
