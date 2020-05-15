136a251c55586064a0443dd34d9fe70bca553bb0
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2020-05-16 01:15:11 +0200

    Add timestamp properties to character and note entities

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/model/Note.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/model/Note.kt
@@ -8,6 +8,8 @@ import javax.persistence.*
 data class Note(@Column(name="contents", columnDefinition = "TEXT") var contents: String,
                 @OneToOne var author: User,
                 @ManyToOne var character: Character,
+                @Column(name="added_on", columnDefinition = "TIMESTAMP WITH TIME ZONE") var addedOn: Date = Date(),
+                @Column(name="edited_on", columnDefinition = "TIMESTAMP WITH TIME ZONE") var editedOn: Date = Date(),
                 @Id @GeneratedValue var id: UUID? = null)
 
 interface NoteRepository: CrudRepository<Note, UUID>
