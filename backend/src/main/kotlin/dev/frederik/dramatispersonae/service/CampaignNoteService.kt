33fe515618341e25ac45352baec49d33594444d5
Frederik Goovaerts <goovaerts.frederik@gmail.com>
2020-06-02 23:14:46 +0200

    Split out duplicated NoteController logic

--- backend/src/main/kotlin/dev/frederik/dramatispersonae/service/CampaignNoteService.kt
+++ backend/src/main/kotlin/dev/frederik/dramatispersonae/service/CampaignNoteService.kt
@@ -1,33 +1,12 @@
 package dev.frederik.dramatispersonae.service
 
-import dev.frederik.dramatispersonae.auth.GoogleAuthentication
 import dev.frederik.dramatispersonae.model.*
-import java.util.*
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/campaignnote")
-class CampaignNoteController(private val service: CampaignNoteService) {
-
-    @PutMapping("/{id}")
-    fun updateNote(
-        auth: GoogleAuthentication,
-        @PathVariable id: UUID,
-        @RequestBody note: CreateNoteDto
-    ): ResponseEntity<Unit> {
-        val success = this.service.updateNote(auth.principal, id, note.contents, note.visibility)
-        return ResponseEntity(if (success) HttpStatus.OK else HttpStatus.FORBIDDEN)
-    }
-
-    @DeleteMapping("/{id}")
-    fun deleteNote(auth: GoogleAuthentication, @PathVariable id: UUID): ResponseEntity<Unit> {
-        val success = this.service.deleteNote(auth.principal, id)
-        return ResponseEntity(if (success) HttpStatus.OK else HttpStatus.FORBIDDEN)
-    }
-}
+class CampaignNoteController(service: CampaignNoteService): NoteController<CampaignNote>(service)
 
 @Component
 class CampaignNoteService(repository: CampaignNoteRepository) : NoteService<CampaignNote>(repository) {
