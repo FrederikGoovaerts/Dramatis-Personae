d95e788d9291817372bb7c0a15f97d5ebde64bdf
Frederik Goovaerts <goovaerts.frederik@gmail.com>
2020-06-02 22:54:57 +0200

    Add actions and sagas to update and delete campaign notes

--- frontend/src/store/actions/note.actions.ts
+++ frontend/src/store/actions/note.actions.ts
@@ -3,12 +3,16 @@ import { ActionTypeMapping, ActionUnion, createAction } from './base';
 
 export enum names {
     editCharacterNote = 'EDIT_CHARACTER_NOTE',
-    deleteCharacterNote = 'DELETE_CHARACTER_NOTE'
+    deleteCharacterNote = 'DELETE_CHARACTER_NOTE',
+    editCampaignNote = 'EDIT_CAMPAIGN_NOTE',
+    deleteCampaignNote = 'DELETE_CAMPAIGN_NOTE'
 }
 
 export const actions = {
     editCharacterNote: (p: EditNotePayload) => createAction(names.editCharacterNote, p),
-    deleteCharacterNote: (p: DeleteNotePayload) => createAction(names.deleteCharacterNote, p)
+    deleteCharacterNote: (p: DeleteNotePayload) => createAction(names.deleteCharacterNote, p),
+    editCampaignNote: (p: EditNotePayload) => createAction(names.editCampaignNote, p),
+    deleteCampaignNote: (p: DeleteNotePayload) => createAction(names.deleteCampaignNote, p)
 };
 
 export type allTypes = ActionUnion<typeof actions>;
