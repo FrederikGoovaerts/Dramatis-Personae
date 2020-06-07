3796ae901e4861f3b396023e0a5b82851dfede98
Frederik Goovaerts <info@frederik.dev>
2020-06-07 14:49:35 +0200

    Fix new property names in migration

--- backend/src/main/resources/db/migration/V1_6__Add_character_labels.sql
+++ backend/src/main/resources/db/migration/V1_6__Add_character_labels.sql
@@ -1,8 +1,8 @@
 create table if not exists label (id uuid not null, is_visible boolean not null, name varchar(255), campaign_id uuid, primary key (id))
 create table if not exists character_label (character_id uuid not null, label_id uuid not null)
 
-alter table campaign add column allow_user_label_management boolean not null default false;
-alter table campaign add column allow_user_character_label_management boolean not null default false;
+alter table campaign add column allow_player_label_management boolean not null default false;
+alter table campaign add column allow_player_character_label_management boolean not null default false;
 
 alter table label add constraint label_campaign_id_ref foreign key (campaign_id) references campaign;
 alter table character_label add constraint character_label_label_id_ref foreign key (label_id) references label;
