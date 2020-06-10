076343c156f27cf46f3e5d8885a2e844012808a1
Frederik Goovaerts <info@frederik.dev>
2020-06-10 23:32:24 +0200

    Extract overlapping labelform logic

--- frontend/src/components/molecules/EditLabelForm.tsx
+++ frontend/src/components/molecules/EditLabelForm.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import { ChangeEvent } from 'react';
-import { Paper, Typography, FormControlLabel, Checkbox, Box } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 
 import { DeleteButton } from '../atoms/DeleteButton';
 import { Label } from '../../types/label.types';
-import { AlertBox } from '../atoms/AlertBox';
+import { LabelForm } from './LabelForm';
 
 interface Props {
     label: Label;
@@ -18,27 +16,9 @@ interface Props {
     deleteLabel: () => void;
 }
 
-interface State {
-    name: string;
-    visible: boolean;
-}
-
-export class EditLabelForm extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = { name: this.props.label.name, visible: this.props.label.visible };
-    }
-
-    handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({ name: event.target.value });
-    };
-
-    handleChangeVisible = (event: ChangeEvent<HTMLInputElement>, checked: boolean) => {
-        this.setState({ visible: checked });
-    };
-
-    handleSubmit = () => {
-        this.props.editLabel(this.state.name, this.state.visible);
+export class EditLabelForm extends React.Component<Props> {
+    handleSubmit = (name: string, visible: boolean) => {
+        this.props.editLabel(name, visible);
         if (this.props.onSubmitComplete) {
             this.props.onSubmitComplete();
         }
@@ -51,8 +31,9 @@ export class EditLabelForm extends React.Component<Props, State> {
         }
     };
 
+    labelOverlaps = (name: string, visible: boolean) => this.props.labelOverlaps(name, visible, this.props.label.id);
+
     render() {
-        const overlap = this.props.labelOverlaps(this.state.name, this.state.visible, this.props.label.id);
         return (
             <Paper className="modalPaper">
                 <div className="modalContainer">
@@ -60,39 +41,13 @@ export class EditLabelForm extends React.Component<Props, State> {
                         <Typography variant="h5">Update label</Typography>
                         <DeleteButton onConfirm={this.handleDelete} />
                     </div>
-                    <TextField
-                        variant="outlined"
-                        label="Name"
-                        helperText="The name of the label, which will be shown when applied to characters."
-                        value={this.state.name}
-                        onChange={this.handleChangeName}
-                        margin="normal"
+                    <LabelForm
+                        label={this.props.label}
+                        owner={this.props.owner}
+                        labelOverlaps={this.labelOverlaps}
+                        submit={this.handleSubmit}
+                        submitLabel="Update"
                     />
-                    {this.props.owner && (
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={this.state.visible}
-                                    onChange={this.handleChangeVisible}
-                                    color="primary"
-                                />
-                            }
-                            label="Visible"
-                        />
-                    )}
-                    {overlap && (
-                        <Box marginY="0.5em">
-                            <AlertBox text="This label overlaps with an existing label." />
-                        </Box>
-                    )}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={this.handleSubmit}
-                        disabled={!this.state.name || overlap}
-                    >
-                        Update
-                    </Button>
                 </div>
             </Paper>
         );
