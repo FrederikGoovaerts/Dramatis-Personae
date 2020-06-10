9a8f8590a97e68bceff5d98196a77a60baac2043
Frederik Goovaerts <info@frederik.dev>
2020-06-10 23:33:43 +0200

    Fix linting error

--- frontend/src/components/molecules/EditLabelForm.tsx
+++ frontend/src/components/molecules/EditLabelForm.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { ChangeEvent } from 'react';
 import { Paper, Typography } from '@material-ui/core';
 
 import { DeleteButton } from '../atoms/DeleteButton';
