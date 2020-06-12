851a678354d03008a55b9993b6c2e13dcbf4427e
Frederik Goovaerts <info@frederik.dev>
2020-06-13 01:38:48 +0200

    Add some basic JPA tests

---
+++ backend/src/test/kotlin/dev/frederik/dramatispersonae/fixtures/TestFixtures.kt
@@ -0,0 +1,30 @@
+package dev.frederik.dramatispersonae.fixtures
+
+import dev.frederik.dramatispersonae.model.*
+
+fun getTestUser(id: String = "1",
+                name: String = "testUser",
+                email: String = "testuser@example.com"): User = User(id, name, email)
+
+fun getTestCampaign(name: String = "testCampaign",
+                    owner: User = getTestUser(),
+                    members: MutableList<User> = mutableListOf(owner)): Campaign = Campaign(name, owner, members)
+
+fun getTestCharacter(name: String = "testCharacter",
+                     description: String = "test description",
+                     isVisible: Boolean = true,
+                     campaign: Campaign = getTestCampaign()): Character = Character(name, description, isVisible, campaign)
+
+fun getTestCharacterNote(contents: String = "test note",
+                         user: User = getTestUser(),
+                         character: Character = getTestCharacter(),
+                         visibility: NoteVisibility = NoteVisibility.PUBLIC): CharacterNote = CharacterNote(contents, user, character, visibility)
+
+fun getTestCampaignNote(contents: String = "test note",
+                        user: User = getTestUser(),
+                        campaign: Campaign = getTestCampaign(),
+                        visibility: NoteVisibility = NoteVisibility.PUBLIC): CampaignNote = CampaignNote(contents, user, campaign, visibility)
+
+fun getTestLabel(name: String = "testLabel",
+                 visibility: Boolean = true,
+                 campaign: Campaign = getTestCampaign()): Label = Label(name, visibility, campaign)
--- backend/src/test/kotlin/dev/frederik/dramatispersonae/model/CharacterTests.kt
+++ backend/src/test/kotlin/dev/frederik/dramatispersonae/model/CharacterTests.kt
@@ -1,26 +1,75 @@
 package dev.frederik.dramatispersonae.model
 
+import dev.frederik.dramatispersonae.fixtures.getTestCharacter
+import dev.frederik.dramatispersonae.fixtures.getTestCharacterNote
+import dev.frederik.dramatispersonae.fixtures.getTestLabel
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
 
 @DataJpaTest
-class CharacterRepositoryTests @Autowired constructor(val entityManager: TestEntityManager,
-                                                      val userRepository: CharacterRepository) {
+@ActiveProfiles("test")
+class CharacterRepositoryTests @Autowired constructor(val userRepository: UserRepository,
+                                                      val campaignRepository: CampaignRepository,
+                                                      val characterRepository: CharacterRepository,
+                                                      val labelRepository: LabelRepository,
+                                                      val noteRepository: CharacterNoteRepository) {
+
+    val character: Character = getTestCharacter();
 
     @BeforeEach
     fun beforeEach() {
-        val dummyChar = Character("dummy name", "a character", true)
-        userRepository.save(dummyChar)
+        userRepository.save(character.campaign.owner)
+        campaignRepository.save(character.campaign)
+        characterRepository.save(character)
     }
 
     @Test
     fun `Should persist a character`() {
         val amount = userRepository.findAll().count()
-        assertThat(amount).isEqualTo(1);
+        assertThat(amount).isEqualTo(1)
+    }
+
+    @Test
+    fun `Should not delete a campaign when deleting a contained character`() {
+        assertThat(campaignRepository.findAll().count()).isEqualTo(1)
+        assertThat(characterRepository.findAll().count()).isEqualTo(1)
+
+        characterRepository.delete(character)
+
+        assertThat(campaignRepository.findAll().count()).isEqualTo(1)
+        assertThat(characterRepository.findAll().count()).isEqualTo(0)
+    }
+
+    @Test
+    fun `Should delete a contained note when deleting a character`() {
+        character.notes.add(getTestCharacterNote(user = character.campaign.owner, character = character))
+        characterRepository.save(character)
+        assertThat(characterRepository.findAll().count()).isEqualTo(1)
+        assertThat(noteRepository.findAll().count()).isEqualTo(1)
+
+        characterRepository.delete(character)
+
+        assertThat(characterRepository.findAll().count()).isEqualTo(0)
+        assertThat(noteRepository.findAll().count()).isEqualTo(0)
+    }
+
+    @Test
+    fun `Should not delete a contained label when deleting a character`() {
+        val label = getTestLabel(campaign = character.campaign)
+        labelRepository.save(label)
+        character.labels.add(label)
+        characterRepository.save(character)
+        assertThat(characterRepository.findAll().count()).isEqualTo(1)
+        assertThat(labelRepository.findAll().count()).isEqualTo(1)
+
+        characterRepository.delete(character)
+
+        assertThat(characterRepository.findAll().count()).isEqualTo(0)
+        assertThat(labelRepository.findAll().count()).isEqualTo(1)
     }
 
 }
