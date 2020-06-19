4d9d19f6db94c0af1a155811742b1f70b4f8bd3d
Frederik Goovaerts <info@frederik.dev>
2020-06-20 01:46:20 +0200

    Add first react rendering test

---
+++ frontend/test/components/atoms/DeleteButton.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { DeleteButton } from '../../../src/components/atoms/DeleteButton';
+import renderer from 'react-test-renderer';
+
+test('The delete button should go through three states', () => {
+    const component = renderer.create(<DeleteButton onConfirm={() => undefined} />);
+    let tree = component.toJSON();
+    if (!tree) {
+        fail();
+    }
+    expect(tree).toMatchSnapshot();
+
+    // Click delete button once
+    tree.props.onClick();
+    tree = component.toJSON();
+    if (!tree) {
+        fail();
+    }
+    expect(tree).toMatchSnapshot();
+
+    // Confirm delete
+    tree.props.onClick();
+    tree = component.toJSON();
+    if (!tree) {
+        fail();
+    }
+    expect(tree).toMatchSnapshot();
+});
