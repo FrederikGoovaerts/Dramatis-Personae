f5ebbd79ad53ace988ebc423e62aee71aab16785
Frederik Goovaerts <info@frederik.dev>
2020-06-21 01:31:13 +0200

    Add Landing component snapshot

---
+++ frontend/test/components/atoms/Landing.test.tsx
@@ -0,0 +1,16 @@
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import { Landing } from '../../../src/components/atoms/Landing';
+
+describe('The Landing', () => {
+    it('should render correctly', () => {
+        const component = create(<Landing />);
+        const tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
+    });
+});
