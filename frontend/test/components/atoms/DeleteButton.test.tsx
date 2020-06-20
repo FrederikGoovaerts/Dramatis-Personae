a6c3971d24bfb84dec6d292a441fa92ec4cb3f4d
Frederik Goovaerts <info@frederik.dev>
2020-06-21 00:10:55 +0200

    Fix structure of other tests

--- frontend/test/components/atoms/DeleteButton.test.tsx
+++ frontend/test/components/atoms/DeleteButton.test.tsx
@@ -2,54 +2,58 @@ import React from 'react';
 import { DeleteButton, ConfirmableButton } from '../../../src/components/atoms/DeleteButton';
 import { create, act } from 'react-test-renderer';
 
-test('The delete button should go through three states', () => {
-    const component = create(<DeleteButton onConfirm={() => undefined} />);
-    let tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+describe('The delete button', () => {
+    it('should go through three states', () => {
+        const component = create(<DeleteButton onConfirm={() => undefined} />);
+        let tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
 
-    // Click delete button once
-    act(tree.props.onClick);
-    tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+        // Click delete button once
+        act(tree.props.onClick);
+        tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
 
-    // Confirm delete
-    act(tree.props.onClick);
-    tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+        // Confirm delete
+        act(tree.props.onClick);
+        tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
+    });
 });
 
-test('The confirmable button should show the supplied labels', () => {
-    const component = create(
-        <ConfirmableButton defaultText="the default" confirmedText="when confirmed" onConfirm={() => undefined} />
-    );
-    let tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+describe('The confirmable button', () => {
+    it('should show the supplied labels', () => {
+        const component = create(
+            <ConfirmableButton defaultText="the default" confirmedText="when confirmed" onConfirm={() => undefined} />
+        );
+        let tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
 
-    // Click delete button once
-    act(tree.props.onClick);
-    tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+        // Click delete button once
+        act(tree.props.onClick);
+        tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
 
-    // Confirm delete
-    act(tree.props.onClick);
-    tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+        // Confirm delete
+        act(tree.props.onClick);
+        tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
+    });
 });
--- frontend/test/components/atoms/ListItemLink.test.tsx
+++ frontend/test/components/atoms/ListItemLink.test.tsx
@@ -5,15 +5,17 @@ import { ListItemLink } from '../../../src/components/atoms/ListItemLink';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 
-test('The ListItemLink should render correctly', () => {
-    const component = create(
-        <Router history={createMemoryHistory()}>
-            <ListItemLink to="destination">contents</ListItemLink>
-        </Router>
-    );
-    const tree = component.toJSON();
-    if (!tree) {
-        fail();
-    }
-    expect(tree).toMatchSnapshot();
+describe('The ListItemLink', () => {
+    it('should render correctly', () => {
+        const component = create(
+            <Router history={createMemoryHistory()}>
+                <ListItemLink to="destination">contents</ListItemLink>
+            </Router>
+        );
+        const tree = component.toJSON();
+        if (!tree) {
+            fail();
+        }
+        expect(tree).toMatchSnapshot();
+    });
 });
