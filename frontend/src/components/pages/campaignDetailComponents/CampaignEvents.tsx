8745e6764493432c0f4194ba4647fa65bd3c8670
Frederik Goovaerts <info@frederik.dev>
2020-12-27 01:20:13 +0100

    Add first create dialog functionality

--- frontend/src/components/pages/campaignDetailComponents/CampaignEvents.tsx
+++ frontend/src/components/pages/campaignDetailComponents/CampaignEvents.tsx
@@ -1,5 +1,6 @@
 import {
     Box,
+    Button,
     CircularProgress,
     Fab,
     IconButton,
@@ -9,6 +10,7 @@ import {
     ListItemText,
     Modal,
     Paper,
+    TextField,
     Typography
 } from '@material-ui/core';
 import Add from '@material-ui/icons/Add';
@@ -19,7 +21,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { eventActions } from '../../../store/actions';
 import { RootState } from '../../../store/reducers';
-import { CreateEventPayload, Event } from '../../../types/event.types';
+import { Event } from '../../../types/event.types';
 
 interface Props {
     campaignId: string;
@@ -30,27 +32,87 @@ export const CampaignEvents = (props: Props) => {
     const events = useSelector((s: RootState) => s.events.events);
     const loading = useSelector((s: RootState) => s.events.loading);
 
+    const [createAt, setCreateAt] = useState<number | undefined>(undefined);
+    const [createName, setCreateName] = useState<string>('');
+    const [createDesc, setCreateDesc] = useState<string>('');
+
     useEffect(() => {
         dispatch(eventActions.actions.getEvents(props.campaignId));
     }, [dispatch, props.campaignId]);
-
-    const renderEvent = (event: Event) => (
-        <ListItem dense={true}>
-            <ListItemText primary={<Typography>{event.name}</Typography>} secondary={event.description} />
-        </ListItem>
-    );
 
     if (loading) {
         return <CircularProgress />;
     }
 
+    const clearCreate = () => {
+        setCreateAt(undefined);
+        setCreateName('');
+        setCreateDesc('');
+    };
+
+    const doCreate = () => {
+        dispatch(
+            eventActions.actions.createEvent({
+                campaignId: props.campaignId,
+                name: createName,
+                description: createDesc
+            })
+        );
+        clearCreate();
+    };
+
+    const renderEvent = (event: Event) => (
+        <ListItem dense={true} key={event.ordinal}>
+            <ListItemText primary={<Typography>{event.name}</Typography>} secondary={event.description} />
+        </ListItem>
+    );
+
+    const renderCreate = () => (
+        <ListItem dense={true} key="create">
+            <Box display="flex" flex={1} justifyContent="space-between" alignItems="center">
+                <Box flexDirection="column" display="flex" flex={1}>
+                    <TextField
+                        value={createName}
+                        size="small"
+                        fullWidth={false}
+                        label="Event title"
+                        onChange={(event) => setCreateName(event.target.value)}
+                    />
+                    <TextField
+                        value={createDesc}
+                        size="small"
+                        fullWidth={true}
+                        label="Event description"
+                        onChange={(event) => setCreateDesc(event.target.value)}
+                    />
+                </Box>
+
+                <Box flexDirection="column" display="flex">
+                    <Button onClick={doCreate}>Create</Button>
+                    <Button onClick={clearCreate}>Cancel</Button>
+                </Box>
+            </Box>
+        </ListItem>
+    );
+
+    const getListToRender = () => {
+        const eventNodes = events.map(renderEvent);
+        if (createAt !== undefined) {
+            eventNodes.splice(createAt, 0, renderCreate());
+        }
+        return eventNodes;
+    };
+
     return (
         <Box>
             <Box marginBottom="1em">
-                {events.length === 0 ? (
-                    <Typography variant="body1">This campaign does not have any events yet.</Typography>
+                {events.length === 0 && createAt === undefined ? (
+                    <>
+                        <Typography variant="body1">This campaign does not have any events yet.</Typography>
+                        <Button onClick={() => setCreateAt(0)}>Create</Button>
+                    </>
                 ) : (
-                    <Paper elevation={3}>{<List>{events.map(renderEvent)}</List>}</Paper>
+                    <Paper elevation={3}>{<List>{getListToRender()}</List>}</Paper>
                 )}
             </Box>
         </Box>
