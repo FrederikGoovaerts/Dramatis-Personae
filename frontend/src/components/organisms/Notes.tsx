faf48715d5a12c9d15fa0c7345a57eb2b50c1435
Frederik Goovaerts <info@frederik.dev>
2021-03-08 11:03:16 +0100

    Simplify note rendering and styling

--- frontend/src/components/organisms/Notes.tsx
+++ frontend/src/components/organisms/Notes.tsx
@@ -1,5 +1,4 @@
-import { LockIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Box, Button, Divider, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Heading, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
 import { Note, NoteVisibility } from '../../types/note.types';
@@ -17,51 +16,43 @@ export const Notes = (props: Props) => {
     const [editNote, setEditNote] = useState<Note | undefined>(undefined);
 
     const renderNote = (note: Note) => {
+        const visibility =
+            note.visibility === 'PUBLIC' ? 'Public' : note.visibility === 'PRIVATE' ? 'Private' : 'Shared with DM';
         return (
-            <Box key={note.id} paddingX="1em">
-                {note.contents.split('\n').map((el, index) => (
-                    <Text key={index}>{el}</Text>
-                ))}
-                {note.visibility === 'PUBLIC' && <ViewIcon />}
-                {note.visibility === 'DM_SHARED' && <LockIcon />}
-                {note.visibility === 'PRIVATE' && <ViewOffIcon />}
-
-                <Button onClick={() => setEditNote(note)}>Edit</Button>
-            </Box>
+            <>
+                <Box key={note.id} padding="1em">
+                    <Flex justify="space-between">
+                        {note.contents.split('\n').map((el, index) => (
+                            <Text key={index}>{el}</Text>
+                        ))}
+                        <Flex direction="column">
+                            <Button onClick={() => setEditNote(note)} marginBottom="0.5em">
+                                Edit
+                            </Button>
+                            <Text fontStyle="italic">{visibility}</Text>
+                        </Flex>
+                    </Flex>
+                </Box>
+                <Divider key={`divider-${note.id}`} />
+            </>
         );
-    };
-
-    const renderNotes = (renderedNotes: JSX.Element[]) => {
-        for (let i = 1; i < renderedNotes.length; i = i + 2) {
-            renderedNotes.splice(i, 0, <Divider key={`divider${i}`} />);
-        }
-        return <Box>{renderedNotes}</Box>;
-    };
-
-    const renderOwnNotes = () => {
-        const renderedNotes = [
-            ...props.notes.map(renderNote),
-            <Box display="flex" flexDirection="row" justifyContent="center" paddingTop="0.5em" key={'addButton'}>
-                <Button onClick={() => setCreateOpen(true)}>Add</Button>
-            </Box>
-        ];
-        return renderNotes(renderedNotes);
     };
 
     return (
         <Box>
-            <Box marginBottom="1em">
+            <Flex marginBottom="1em" alignItems="center">
                 <Heading>Your notes</Heading>
-            </Box>
-            <Box marginBottom="2em">{renderOwnNotes()}</Box>
+                <Button onClick={() => setCreateOpen(true)} size="sm" marginLeft="1em">
+                    Add
+                </Button>
+            </Flex>
+            <Box marginBottom="2em">{props.notes.map(renderNote)}</Box>
             {props.sharedNotes.length > 0 && (
                 <Box marginBottom="1em">
                     <Heading>Notes by others</Heading>
                 </Box>
             )}
-            {props.sharedNotes.length > 0 && (
-                <Box marginBottom="2em">{renderNotes(props.sharedNotes.map(renderNote))}</Box>
-            )}
+            {props.sharedNotes.length > 0 && <Box marginBottom="2em">{props.sharedNotes.map(renderNote)}</Box>}
         </Box>
     );
 };
